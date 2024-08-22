7210aef3c9553a502e274f6b9f7a80a3e62193b8
MA <modupe18ab@gmail.com>
2024-08-22 14:56:12 -0500

    first commit

---
+++ rat.sh
@@ -0,0 +1,14 @@
+Steps:
+Make sure you have your project in your machine
+Create a repository in the GitHub
+Create a local repository -> open git bash -> enter git init 
+Add the project files to the staging area with the command git add .
+Save the files from the staging area to the local repository with the command git commit -m “commit message”
+ M stands for message
+The commit message should be meaningful.
+If you are using the git for the first time, then you have to introduce yourself to the git.
+git config –global user.name “your github username”
+git config -global user.email “your github mail id”
+You have to connect your local repository and the remote repository
+You can go ahead and push the files from the local repository to the remote repository
+
